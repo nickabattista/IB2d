43b39f9bac56f5d6b95c0422398084f16da4318c
Nick Battista <nick_battista@Laura-Millers-MacBook-Pro.local>
2015-07-15 23:18:24 -0400

    added comments into new masive immersed boundary functions

--- IBM_Blackbox/please_Update_Massive_Boundary_Velocity.m
+++ IBM_Blackbox/please_Update_Massive_Boundary_Velocity.m
@@ -1,3 +1,40 @@
+%*****************************************************************************%
+%***********************************% IB2d %**********************************%
+%*****************************************************************************%
+
+% IB2d is an Immersed Boundary Code (IB) for solving fully coupled non-linear 
+%  	fluid-structure interaction models. This version of the code is based 
+% 	off of Peskin's Immersed Boundary Method Paper in Acta Numerica, 2002.
+%
+% Author: Nicholas A. Battista
+% Email:  nick.battista@unc.edu
+% Date Created: May 27th, 2015
+% Institution: University of North Carolina at Chapel Hill
+% Website: http://battista.web.unc.edu
+% GitHub: http://www.github.com/nickabattista
+% 
+% This code is capable of creating Lagrangian Structures using:
+%  	1. Springs
+%  	2. Beams (*torsional springs)
+%  	3. Target Points
+% 	4. Muscle-Model (combined Force-Length-Velocity model, "HIll+(Length-
+%                         Tension)")
+%         5. Mass Points
+% 
+% One is able to update those Lagrangian Structure Parameters, e.g., spring constants, resting lengths, etc
+%  
+% There are a number of built in Examples, mostly used for teaching purposes. 
+% 
+% If you would like us to add a specific muscle model, please contact Nick (nick.battista@unc.edu) 
+% 
+% If you use this code for the purposes of teaching, research, or recreation please let Nick know as well :)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: update 'massive' immersed boundary velocity
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function mVelocity_h = please_Update_Massive_Boundary_Velocity(dt_step,mass_info,mVelocity,F_Mass_Bnd)
 
 % dt_step: desired time-step for this position
