416787afb871936f34b2b428bcd8f0298ee7c489
Nick Battista <nick_battista@math00197.dhcp.unc.edu>
2015-07-28 16:29:27 -0400

    perturb porous example comparing porous rubberband vs non-porous

--- Examples/Example_Porous_Rubberbands/Compare_Porous_NonPorous_Rubberbands/Rubberband.m
+++ Examples/Example_Porous_Rubberbands/Compare_Porous_NonPorous_Rubberbands/Rubberband.m
@@ -76,7 +76,7 @@ k_Spring = 1e7;
 print_Lagrangian_Springs(xLag,yLag,k_Spring,ds_Rest,struct_name);
 
 % Prints .porous file!
-alpha = 5e-5; 
+alpha = 1e-3; 
 print_Lagrangian_Porosity(xLag,alpha,struct_name)
 
 
