7aff8d2c0c4271b379f1177660013989ad261886
Ao Zeng <13871299198@msn.cn>
2016-05-20 14:37:03 -0400

    reviesed test code

--- pyIB2d/src/test/test.cpp
+++ pyIB2d/src/test/test.cpp
@@ -1,45 +1,43 @@
 #include <stdio.h>
 
 
-double** add(double** array1, double** array2, int row1, int col1, int row2, int col2){
+void add(double* array1, double* array2, int row1, int col1, int row2, int col2){
+    int index =0;
     for (int i=0;i<row1;i++){
         for (int j = 0; j<col1;j++){
-            array1[i][j] = array2[i][j]+array1[i][j];
+            array1[index] = array1[index]+array2[index];
+            index++;
         }
-        printf("\n");
     }
-    return array1;
 }
 
 int main(){
-    double** result;
-    double** x1;
-    double** x2;
-    x1= new double *[10];
-    for(int i = 0; i <10; i++){
-        x1[i] = new double[3];
-    }
-    for (int i=0;i<10;i++){
-        for (int j = 0; j<3;j++)
-            x1[i][j] = i*3+j;
-    }
-    x2 = new double *[10];
-    for(int i = 0; i <10; i++){
-        x2[i] = new double[3];
-    }
-    for (int i=0;i<10;i++){
-        for (int j = 0; j<3;j++)
-            x2[i][j] = i*3+j;
+    double x1[30];
+    double x2[30];
+    int index1 = 0;
+    int index2 = 0;
+    for (int i=0; i< 10; i++){
+        for (int j=0; j<3;j++){
+            x1[index1] = index1;
+            index1++;
+        }
     }
     
-    result = add(x1,x2,10,3,10,3);
+    for (int i=0; i< 10; i++){
+        for (int j=0; j<3;j++){
+            x2[index2] = index2;
+            index2++;
+        }
+    }
     
-    for (int i=0;i<10;i++){
-        for (int j = 0; j<3;j++){
-            printf("%f ",result[i][j]);
+    add(x1,x2,10,3,10,3);
+    for (int i=0; i< 10;i++){
+        for (int j=0;j<3;j++){
+            printf("%f", x1[i*3+j]);
         }
         printf("\n");
     }
+    
     return 0;
 }
 
