2c894de67dead7f201cef62e20cdb5af4a37f45d
Ao Zeng <13871299198@msn.cn>
2016-05-23 13:17:12 -0400

    add new modified files

--- pyIB2d/src/example2/setup.py
+++ pyIB2d/src/example2/setup.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 import numpy
 
-ext_modules = [Extension("test", ["test.pyx", "printlist.cpp"], include_dirs=[numpy.get_include()], 
-                language='c++',)]
-
-setup(cmdclass = {'build_ext': build_ext}, ext_modules = ext_modules)
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("write",
+                             sources=["write.pyx", "c_write.c"],
+                             include_dirs=[numpy.get_include()])],
+)
--- pyIB2d/src/example2/test.py
+++ pyIB2d/src/example2/test.py
@@ -1,4 +1,18 @@
-from test import list_test
+#!/usr/bin/env python
+
+"""
+simple test of the multiply.pyx and c_multiply.c test code
+"""
+
+
 import numpy as np
 
-list_test(np.array([1,2,3]))
+import write
+
+a = np.array([[1.0, 2.0], [3.0, 4.0]])
+
+print (a)
+
+write.write(a)
+
+print (a)
