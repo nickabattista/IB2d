f45a5da8638855a6dbda134d3b90718266264d46
Ao Zeng <13871299198@msn.cn>
2016-05-31 14:32:20 -0400

    c_savevtk_points_write method called by test_wrtFile.py working example

--- pyIB2d/src/test_wrtFile.py
+++ pyIB2d/src/test_wrtFile.py
@@ -3,16 +3,18 @@
 '''Python file to test c_wrtFile.c and wrtFile.pyx'''
 
 import numpy as np
-import wrtFile
+import write
 
 A = np.array([[1.,2.,3.],[4.,5.,6.]])
 Arow = A.shape[0]
 Acol = A.shape[1]
 
-B = np.random.rand(3,4)
+B = np.random.rand(2,3)
 Brow = B.shape[0]
 Bcol = B.shape[1]
 
 filename = 'itworks.txt'
 
 vecname = 'foo'
+
+write.write(Arow, A,filename, vecname)
