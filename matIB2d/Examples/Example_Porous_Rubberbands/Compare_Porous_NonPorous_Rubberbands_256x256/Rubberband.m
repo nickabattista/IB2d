91838b1090f456179926a7d4f365ba1d1bc7b78e
Nick Battista <nickabattista@gmail.com>
2016-09-17 16:20:03 -0400

    updated comparing porous to non-porous rubberband example with higher resolution

---
+++ matIB2d/Examples/Example_Porous_Rubberbands/Compare_Porous_NonPorous_Rubberbands_256x256/Rubberband.m
@@ -0,0 +1,257 @@
+%-------------------------------------------------------------------------------------------------------------------%
+%
+% IB2d is an Immersed Boundary Code (IB) for solving fully coupled non-linear 
+% 	fluid-structure interaction models. This version of the code is based off of
+%	Peskin's Immersed Boundary Method Paper in Acta Numerica, 2002.
+%
+% Author: Nicholas A. Battista
+% Email:  nick.battista@unc.edu
+% Date Created: May 27th, 2015
+% Institution: UNC-CH
+%
+% This code is capable of creating Lagrangian Structures using:
+% 	1. Springs
+% 	2. Beams (*torsional springs)
+% 	3. Target Points
+%	4. Muscle-Model (combined Force-Length-Velocity model, "HIll+(Length-Tension)")
+%
+% One is able to update those Lagrangian Structure parameters, e.g., spring constants, resting %%	lengths, etc
+% 
+% There are a number of built in Examples, mostly used for teaching purposes. 
+% 
+% If you would like us %to add a specific muscle model, please let Nick (nick.battista@unc.edu) know.
+%
+%--------------------------------------------------------------------------------------------------------------------%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: creates the RUBBERBAND-EXAMPLE geometry and prints associated input files
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function Rubberband()
+
+%
+% Grid Parameters (MAKE SURE MATCHES IN input2d !!!)
+%
+Nx =  256;       % # of Eulerian Grid Pts. in x-Direction (MUST BE EVEN!!!)
+Ny =  256;       % # of Eulerian Grid Pts. in y-Direction (MUST BE EVEN!!!)
+Lx = 2.0;        % Length of Eulerian Grid in x-Direction
+Ly = 2.0;        % Length of Eulerian Grid in y-Direction
+
+
+% Immersed Structure Geometric / Dynamic Parameters %
+N = 2*Nx;        % Number of Lagrangian Pts. (2x resolution of Eulerian grid)
+a = 0.4;         % Length of semi-major axis.
+b = 0.2;         % Length of semi-minor axis.
+ds_Rest = 0;     % Resting length of springs
+struct_name = 'rubberband'; % Name for .vertex, .spring, etc files.
+
+
+% Call function to construct geometry
+[xLag,yLag] = give_Me_Immsersed_Boundary_Geometry(N,a,b);
+
+% Construct Second Rubberband
+xLag2 = xLag + 1.0;
+yLag2 = yLag;
+
+
+% Plot Geometry to test
+plot(xLag,yLag,'r-'); hold on;
+plot(xLag,yLag,'*'); hold on;
+plot(xLag2,yLag2,'k-'); hold on;
+plot(xLag2,yLag2,'m*'); hold on;
+xlabel('x'); ylabel('y');
+axis square;
+
+xLags = [xLag xLag2];
+yLags = [yLag yLag2];
+
+% Prints .vertex file!
+print_Lagrangian_Vertices(xLags,yLags,struct_name);
+
+
+% Prints .spring file!
+k_Spring = 2e7;
+print_Lagrangian_Springs(xLag,yLag,k_Spring,ds_Rest,struct_name);
+
+% Prints .porous file!
+alpha = 3.5e-4; 
+print_Lagrangian_Porosity(xLag,alpha,struct_name)
+
+
+% Prints .beam file!
+%k_Beam = 0.5; C = 0.0;
+%print_Lagrangian_Beams(xLag,yLag,k_Beam,C,struct_name);
+
+
+% Prints .target file!
+%k_Target = 1e7;
+%print_Lagrangian_Target_Pts(xLag,k_Target,struct_name);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: prints VERTEX points to a file called rubberband.vertex
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function print_Lagrangian_Vertices(xLag,yLag,struct_name)
+
+    N = length(xLag);
+
+    vertex_fid = fopen([struct_name '.vertex'], 'w');
+
+    fprintf(vertex_fid, '%d\n', N );
+
+    %Loops over all Lagrangian Pts.
+    for s = 1:N
+        X_v = xLag(s);
+        Y_v = yLag(s);
+        fprintf(vertex_fid, '%1.16e %1.16e\n', X_v, Y_v);
+    end
+
+    fclose(vertex_fid); 
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: prints POROSITY points to a file called rubberband.porous
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function print_Lagrangian_Porosity(xLag,alpha,struct_name)
+
+    %ONLY ONE RUBBERBAND IS POROUS!
+
+    N = length(xLag);
+
+    porous_fid = fopen([struct_name '.porous'], 'w');
+
+    fprintf(porous_fid, '%d\n', N );
+
+    %Loops over all Lagrangian Pts.
+    for s = 1:N
+        if s == 1
+            fprintf(porous_fid, '%d %1.16e %1.16e\n', s, alpha,-2);      
+        elseif s == 2
+            fprintf(porous_fid, '%d %1.16e %1.16e\n', s, alpha,-1);
+        elseif s==N-1
+            fprintf(porous_fid, '%d %1.16e %1.16e\n', s, alpha,1);
+        elseif s==N
+            fprintf(porous_fid, '%d %1.16e %1.16e\n', s, alpha,2);
+        else
+            fprintf(porous_fid, '%d %1.16e %1.16e\n', s, alpha,0);
+        end
+    end
+
+    fclose(porous_fid); 
+        
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: prints TARGET points to a file called rubberband.target
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function print_Lagrangian_Target_Pts(xLag,k_Target,struct_name)
+
+    N = length(xLag);
+
+    target_fid = fopen([struct_name '.target'], 'w');
+
+    fprintf(target_fid, '%d\n', N );
+
+    %Loops over all Lagrangian Pts.
+    for s = 1:N
+        fprintf(target_fid, '%d %1.16e\n', s, k_Target);
+    end
+
+    fclose(target_fid); 
+    
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: prints BEAM (Torsional Spring) points to a file called rubberband.beam
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function print_Lagrangian_Beams(xLag,yLag,k_Beam,C,struct_name)
+
+    % k_Beam: beam stiffness
+    % C: beam curvature
+    
+    N = length(xLag); % NOTE: Total number of beams = Number of Total Lag Pts. - 2
+
+    beam_fid = fopen([struct_name '.beam'], 'w');
+
+    fprintf(beam_fid, '%d\n', N );
+
+    %spring_force = kappa_spring*ds/(ds^2);
+
+    %BEAMS BETWEEN VERTICES
+    for s = 2:N-1
+            if  s <= N-1         
+                fprintf(beam_fid, '%d %d %d %1.16e %1.16e\n',s-1, s, s+1, k_Beam, C);  
+            else
+                %Case s=N
+                fprintf(beam_fid, '%d %d %d %1.16e %1.16e\n',s-1, s, 1,   k_Beam, C);  
+            end
+    end
+    fclose(beam_fid); 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: prints SPRING points to a file called rubberband.spring
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function print_Lagrangian_Springs(xLag,yLag,k_Spring,ds_Rest,struct_name)
+
+    N = length(xLag);
+
+    spring_fid = fopen([struct_name '.spring'], 'w');
+
+    fprintf(spring_fid, '%d\n', 2*N );
+
+    %spring_force = kappa_spring*ds/(ds^2);
+
+    %SPRINGS BETWEEN VERTICES
+    for s = 1:N
+            if s < N         
+                fprintf(spring_fid, '%d %d %1.16e %1.16e\n', s, s+1, k_Spring, ds_Rest);  
+            else
+                %Case s=N
+                fprintf(spring_fid, '%d %d %1.16e %1.16e\n', s, 1,   k_Spring, ds_Rest);  
+            end
+    end
+    
+    for s=N+1:2*N
+            if s < 2*N         
+                fprintf(spring_fid, '%d %d %1.16e %1.16e\n', s, s+1, k_Spring, ds_Rest);  
+            else
+                %Case s=2N
+                fprintf(spring_fid, '%d %d %1.16e %1.16e\n', s, N+1,   k_Spring, ds_Rest);  
+            end
+        
+    end
+    fclose(spring_fid); 
+    
+    
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: creates the Lagrangian structure geometry
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [xLag,yLag] = give_Me_Immsersed_Boundary_Geometry(N,rmin,rmax)
+
+% The immsersed structure is an ellipse %
+t = 2*pi/(2*N);
+for i=1:N
+    
+    xLag(i) = 0.5 + rmax * cos( 2*pi/N*(i-1) );
+    yLag(i) = 1.0 + rmin * sin( 2*pi/N*(i-1) );
+    
+end
+
