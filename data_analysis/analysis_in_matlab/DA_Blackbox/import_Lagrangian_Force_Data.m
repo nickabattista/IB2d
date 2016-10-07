836a6fc523cd887231cc277561bcb2fbe4ee33cd
Nick Battista <nickabattista@gmail.com>
2016-10-07 12:23:03 -0400

    updated blackbox for reading in Lagrangian forces in x,y direction

--- data_analysis/analysis_in_matlab/DA_Blackbox/import_Lagrangian_Force_Data.m
+++ data_analysis/analysis_in_matlab/DA_Blackbox/import_Lagrangian_Force_Data.m
@@ -29,7 +29,15 @@
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [fLagMag,fLagNorm,fLagTan] = import_Lagrangian_Force_Data(path,numSim)
+function [fX_Lag,fY_Lag,fLagMag,fLagNorm,fLagTan] = import_Lagrangian_Force_Data(path,numSim)
+
+% read in Mag. of Force %
+strChoice = 'fX_Lag'; 
+fX_Lag =  read_Force_Scalar_Data_From_vtk(path,numSim,strChoice);
+
+% read in Mag. of Tangential Force %
+strChoice = 'fY_Lag'; 
+fY_Lag = read_Force_Scalar_Data_From_vtk(path,numSim,strChoice);
 
 % read in Mag. of Force %
 strChoice = 'fMag'; 
