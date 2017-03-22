c4b2008343fa5c5582f4e4b3ba8d6c03b0533137
Nick Battista <nickabattista@gmail.com>
2017-03-22 14:30:21 -0400

    first commit for beehive

---
+++ matIB2d/Examples/Examples_First_Year_Seminar/Beehive/update_Target_Point_Positions.m
@@ -0,0 +1,136 @@
+%-------------------------------------------------------------------------------------------------------------------%
+%
+% IB2d is an Immersed Boundary Code (IB) for solving fully coupled non-linear 
+% 	fluid-structure interaction models. This version of the code is based off of
+%	Peskin's Immersed Boundary Method Paper in Acta Numerica, 2002.
+%
+% Author: Nicholas A. Battista
+% Email:  nick.battista@unc.edu
+% Date Created: May 27th, 2015
+% Institution: UNC-CH
+%
+% This code is capable of creating Lagrangian Structures using:
+% 	1. Springs
+% 	2. Beams (*torsional springs)
+% 	3. Target Points
+%	4. Muscle-Model (combined Force-Length-Velocity model, "Hill+(Length-Tension)")
+%
+% One is able to update those Lagrangian Structure parameters, e.g., spring constants, resting lengths, etc
+% 
+% There are a number of built in Examples, mostly used for teaching purposes. 
+% 
+% If you would like us to add a specific muscle model, please let Nick (nick.battista@unc.edu) know.
+%
+%--------------------------------------------------------------------------------------------------------------------%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: updates the target point positions
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function targets = update_Target_Point_Positions(dt,current_time,targets)
+
+% INFO FROM TARGET POINTS %
+IDs = targets(:,1);                 % Stores Lag-Pt IDs in col vector
+xPts= targets(:,2);                 % Original x-Values of x-Target Pts.
+yPts= targets(:,3);                 % Original y-Values of y-Target Pts.
+kStiffs = targets(:,4);             % Stores Target Stiffnesses 
+
+
+% -> FINDING NEXT ANGULAR FREQUENCY <- % 
+angInput = give_Me_Oscillatory_Angle(dt,current_time);
+
+% Declare how open you want the movements to go
+full_ang = pi/3;
+dbl_ang = 2*full_ang;
+
+% Find associated angle
+ang = mod( angInput, dbl_ang );
+if ( (ang > full_ang ) && (ang <= dbl_ang) )
+    ang = dbl_ang-ang;
+end
+
+% Read In REFERENCE Pts!
+[xRef,yRef] = read_File_In('hive.vertex');
+
+% Store Values for Centers of Rotation
+xL = xRef(1);       yL = yRef(1);
+xR = xRef(end/2+1); yR = yRef(end/2+1);
+
+% Rotate Geometry
+[xR_Ref,yR_Ref] = rotate_Geometry(-ang,xR,yR,xRef(end/2+1:end),yRef(end/2+1:end) );
+[xL_Ref,yL_Ref] = rotate_Geometry(ang,xL,yL,xRef(1:end/2),yRef(1:end/2) );
+
+% Store New Geometry
+targets(IDs,2) = [xL_Ref xR_Ref]; 
+targets(IDs,3) = [yL_Ref yR_Ref]; 
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: rotate geometry
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [x,y] = rotate_Geometry(ang,xC,yC,xRef,yRef)
+
+len = length(xRef);
+x = zeros(1,len); y=x;
+
+xRef = xRef - xC;
+yRef = yRef - yC;
+
+for i=1:len
+   x(i) =  xRef(i)*cos(ang) - yRef(i)*sin(ang);
+   y(i) =  xRef(i)*sin(ang) + yRef(i)*cos(ang);
+end
+
+x = x + xC;
+y = y + yC;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: reads in info from file
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [x1,y1] = read_File_In(file_name)
+
+filename = file_name;  %Name of file to read in
+
+fileID = fopen(filename);
+
+    % Read in the file, use 'CollectOutput' to gather all similar data together
+    % and 'CommentStyle' to to end and be able to skip lines in file.
+    C = textscan(fileID,'%f %f','CollectOutput',1);
+
+fclose(fileID);        %Close the data file.
+
+mat_info = C{1};   %Stores all read in data
+
+%Store all elements in matrix
+mat = mat_info(2:end,1:end);
+
+x1 =  mat(:,1); %store xVals 
+y1 =  mat(:,2); %store yVals
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% FUNCTION: gives me next angle
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function angInput = give_Me_Oscillatory_Angle(dt,current_time)
+
+
+dtheta = pi*dt;
+angVec = dtheta:dtheta:2*pi;
+n = ceil((current_time+dt) / dt);
+n = mod(n,length(angVec));
+
+angInput = angVec(n);
