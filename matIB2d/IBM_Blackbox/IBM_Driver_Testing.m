1b8a82354c676ec074bef3c8409d711558b88732
Nick Battista <nickabattista@gmail.com>
2017-04-17 11:16:24 -0400

    updated please_Update_Fluid_Velocity and IBM_Driver files, namely creating meshgrids using the meshgrid function and scalar plotting in vtk so IB2d can handle non-square grids

--- matIB2d/IBM_Blackbox/IBM_Driver_Testing.m
+++ matIB2d/IBM_Blackbox/IBM_Driver_Testing.m
@@ -1072,14 +1072,14 @@ function savevtk_scalar(array, filename, colorMap,dx,dy)
 %  savevtk Save a 3-D scalar array in VTK format.
 %  savevtk(array, filename) saves a 3-D array of any size to
 %  filename in VTK format.
-    [nx, ny, nz] = size(array);
+    [ny, nx, nz] = size(array);
     fid = fopen(filename, 'wt');
     fprintf(fid, '# vtk DataFile Version 2.0\n');
     fprintf(fid, 'Comment goes here\n');
     fprintf(fid, 'ASCII\n');
     fprintf(fid, '\n');
     fprintf(fid, 'DATASET STRUCTURED_POINTS\n');
-    fprintf(fid, 'DIMENSIONS    %d   %d   %d\n', nx, ny, nz);
+    fprintf(fid, 'DIMENSIONS    %d   %d   %d\n', ny, nx, nz);
     fprintf(fid, '\n');
     fprintf(fid, 'ORIGIN    0.000   0.000   0.000\n');
     %fprintf(fid, 'SPACING   1.000   1.000   1.000\n'); if want [1,32]x[1,32] rather than [0,Lx]x[0,Ly]
@@ -1090,8 +1090,8 @@ function savevtk_scalar(array, filename, colorMap,dx,dy)
     fprintf(fid, 'LOOKUP_TABLE default\n');
     fprintf(fid, '\n');
     for a=1:nz
-        for b=1:ny
-            for c=1:nx
+        for b=1:nx
+            for c=1:ny
                 fprintf(fid, '%d ', array(c,b,a));
             end
             fprintf(fid, '\n');
