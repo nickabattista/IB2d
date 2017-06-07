e6c1e29ba6443c502ef11a5a54deeead5214524c
Nick Battista <nickabattista@gmail.com>
2017-06-07 17:16:50 -0400

    added functionality to choose what Eulerian data gets analyzed in data analysis package

--- data_analysis/analysis_in_python/DA_Blackbox/import_Eulerian_Data.py
+++ data_analysis/analysis_in_python/DA_Blackbox/import_Eulerian_Data.py
@@ -23,7 +23,7 @@
 #
 #--------------------------------------------------------------------------------------------------------------------#
 
-#import numpy as np
+import numpy as np
 #import sys
 #import os
 from read_vtk_data import read_Eulerian_Data_From_vtk
@@ -34,39 +34,83 @@ from read_vtk_data import read_Eulerian_Data_From_vtk
 #          
 ################################################################################
 
-def import_Eulerian_Data(path,numSim):
+def import_Eulerian_Data(path,numSim,Eulerian_Flags):
  
+        #
+        # EULERIAN FLAGS FOR WHAT GETS SPIT OUT #
+        # 
+        #Eulerian_Flags(0):   OMEGA
+        #Eulerian_Flags(1):   PRESSURE
+        #Eulerian_Flags(2):   uMAG
+        #Eulerian_Flags(3):   uX (mag. x-component of velocity)
+        #Eulerian_Flags(4):   uY (mag. x-component of velocity)
+        #Eulerian_Flags(5):   uVEC (x,y-components of velocity: U,V)
+        #Eulerian_Flags(6):   Fx (x-component of force )
+        #Eulerian_Flags(7):   Fy (y-component of force)
+        #
+
     # read in Vorticity #
-    strChoice = 'Omega'; getxy = True
-    Omega,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[0]:
+        strChoice = 'Omega'; getxy = True
+        Omega,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        Omega=[]
 
     # read in Pressure #
-    strChoice = 'P'; getxy = False
-    P = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[1]:
+        strChoice = 'P'; getxy = True
+        P,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        P = []
 
     # read in Velocity Magnitude #
-    strChoice = 'uMag'; getxy = False
-    uMag = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[2]:
+        strChoice = 'uMag'; getxy = True
+        uMag,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        uMag = []
  
     # read in x-directed Velocity Magnitude #
-    strChoice = 'uX'; getxy = False
-    uX = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[3]:
+        strChoice = 'uX'; getxy = True
+        uX,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        uX = []
  
     # read in y-directed Velocity Magnitude #
-    strChoice = 'uY'; getxy = False
-    uY = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[4]:
+        strChoice = 'uY'; getxy = True
+        uY,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        uY = []
 
     # read in x-directed Forces #
-    strChoice = 'Fx'; getxy = False
-    Fx = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[6]:
+        strChoice = 'Fx'; getxy = True
+        Fx,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        Fx = []
 
     # read in y-directed Forces #
-    strChoice = 'Fy'; getxy = False
-    Fy = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[7]:
+        strChoice = 'Fy'; getxy = True
+        Fy,x,y = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        Fy = []
  
     # read in Velocity Field #
-    strChoice = 'u'; getxy = False
-    U,V = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    if Eulerian_Flags[5]:
+        strChoice = 'u'; getxy = True
+        U,V = read_Eulerian_Data_From_vtk(path,numSim,strChoice,getxy)
+    else:
+        U = []
+        V = []
+
+    # default for x,y values
+    inds = np.array([0,1,2,3,4,6,7])
+    if max(Eulerian_Flags[inds])==0:
+        x=[]
+        y=[]
 
     return x,y,Omega,P,uMag,uX,uY,U,V,Fx,Fy
 
