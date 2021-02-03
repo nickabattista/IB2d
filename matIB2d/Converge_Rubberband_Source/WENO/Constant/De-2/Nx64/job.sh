f75d45e7cffdf0c3a8de04e6f77cb6b9df1789f3
msantiago2 <malvarado27@ucmerced.edu>
2021-02-03 11:51:21 -0800

    added rubberband convergence

---
+++ matIB2d/Converge_Rubberband_Source/WENO/Constant/De-2/Nx64/job.sh
@@ -0,0 +1,2 @@
+/opt/MATLAB/R2016b/bin/matlab -nosplash -nodisplay -singleCompThread -r "run Rubberband.m; exit;"
+/opt/MATLAB/R2016b/bin/matlab -nosplash -nodisplay -singleCompThread -r "run main2d.m; exit;"
