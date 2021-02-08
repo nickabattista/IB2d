533df469705bbfecc05c6106f8cfbab2ece5f66d
msantiago2 <malvarado27@ucmerced.edu>
2021-02-08 12:05:50 -0800

    deleted convergence study in master branch

--- matIB2d/Converge_Rubberband_Source/Upwind/Limited/De-3/Nx128/job.sh
+++
@@ -1,2 +0,0 @@
-/opt/MATLAB/R2016b/bin/matlab -nosplash -nodisplay -singleCompThread -r "run Rubberband.m; exit;"
-/opt/MATLAB/R2016b/bin/matlab -nosplash -nodisplay -singleCompThread -r "run main2d.m; exit;"
