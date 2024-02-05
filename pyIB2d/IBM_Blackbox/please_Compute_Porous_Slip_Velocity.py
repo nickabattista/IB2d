46f9344c439a4858625a24fa520294c344d54933
nickabattista <nickabattista@gmail.com>
2024-02-05 18:02:14 -0500

    Similar to matIB2d implementation, sped up the fluid solver and force spreading operations by using more efficient numerical linear algebra and vectorized processes; updated code header information

--- pyIB2d/IBM_Blackbox/please_Compute_Porous_Slip_Velocity.py
+++ pyIB2d/IBM_Blackbox/please_Compute_Porous_Slip_Velocity.py
@@ -1,30 +1,25 @@
-'''-------------------------------------------------------------------------
+'''-------------------------------------------------------------------------------------------------
 
  IB2d is an Immersed Boundary Code (IB) for solving fully coupled non-linear 
- 	fluid-structure interaction models. This version of the code is based off of
-	Peskin's Immersed Boundary Method Paper in Acta Numerica, 2002.
+ 	  fluid-structure interaction models. This version of the code is based off of
+	  Peskin's Immersed Boundary Method Paper in Acta Numerica, 2002.
 
  Author: Nicholas A. Battista
- Email:  nick.battista@unc.edu
- Date Created: May 27th, 2015\
+ Email:  battistn[@]tcnj[.]edu
+ IB2d was Created: May 27th, 2015 at UNC-CH
  Initial Python 3.5 port by: Christopher Strickland
- Institution: UNC-CH
 
  This code is capable of creating Lagrangian Structures using:
  	1. Springs
  	2. Beams (*torsional springs)
  	3. Target Points
-	4. Muscle-Model (combined Force-Length-Velocity model, "HIll+(Length-Tension)")
-
- One is able to update those Lagrangian Structure parameters, e.g., 
- spring constants, resting lengths, etc
+ 	4. Muscle-Model (combined Force-Length-Velocity model, "Hill+(Length-Tension)")
+    .
+    .
  
  There are a number of built in Examples, mostly used for teaching purposes. 
- 
- If you would like us to add a specific muscle model, 
- please let Nick (nick.battista@unc.edu) know.
 
-----------------------------------------------------------------------------'''
+----------------------------------------------------------------------------------------------------'''
 
 import numpy as np
 from math import sqrt
